5c384b84d2cce6542916261d04fd4a1b9a93320c
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-07-08 09:12:31 -0300

    add dbeaver install

--- flatpak-install.sh
+++ flatpak-install.sh
@@ -1,5 +1,6 @@
 #!bin/bash
 
 bash scripts/flatpak/atom-install.sh
+bash scripts/flatpak/dbeaver-install.sh
 bash scripts/flatpak/spotify-install.sh
 bash scripts/flatpak/visual-studio-code-install.sh
