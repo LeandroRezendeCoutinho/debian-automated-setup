8e0a9f49a57a56953a7cb6aafff1185303b55228
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-09-27 11:28:50 -0300

    add node and yarn

---
+++ environment-setup.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+
+bash scripts/setup/asdf-setup.sh
+bash scripts/setup/git-setup.sh
+bash scripts/setup/ruby-setup.sh
+bash scripts/setup/tmux-mem-cpu-load-setup.sh
+bash scripts/setup/yarn-setup.sh
+bash scripts/setup/fish-setup.sh
