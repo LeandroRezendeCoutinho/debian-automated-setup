f85c36e9230e099d9e5077c1cbdfef9b9f0aff8c
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-09-27 13:48:43 -0300

    add wine 32bits install

--- scripts/apt/wine-install.sh
+++ scripts/apt/wine-install.sh
@@ -6,4 +6,13 @@ then
     exit 1
 fi
 
-apt install --yes wine
+dpkg --add-architecture i386 
+
+apt update
+
+apt install --yes wine 
+apt install --yes wine32 
+apt install --yes wine64 
+apt install --yes libwine 
+apt install --yes libwine:i386 
+apt install --yes fonts-wine
