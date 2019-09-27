0f9ee5522d5f507672805c6adc56ab4cea7e6b1b
leandrorezendecoutinho <leandro.rc.contato@gmail.com>
2019-09-27 11:39:03 -0300

    add mono install

--- apt-install.sh
+++ apt-install.sh
@@ -9,6 +9,7 @@ fi
 bash scripts/apt/build-essential-install.sh
 bash scripts/apt/curl-install.sh
 bash scripts/apt/fish-install.sh
+bash scripts/apt/mono-install.sh
 bash scripts/apt/google-chrome-install.sh
 bash scripts/apt/tmux-install.sh
 bash scripts/apt/wine-install.sh
